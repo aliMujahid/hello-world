755a8ad7885bcdc912c21239b5db5b4f338243ce
aliMujahid <mhmmdalimujahid@gmail.com>
2019-04-16 11:48:31 +0600

    Request for user_agent

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/<name>')
 def user(name):
-    return f"<h1> Hello, {name}</h1>"
+    user_agent = request.headers.get('user_agent')
+    return f"<h1> Hello, {name}</h1><p> Your browser is {user_agent}.</p>"
 
 @app.route('/')
 def index():
     return f"<h1>Hello world.</h1>"
+
+    
