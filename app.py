bf8401e7b1fac1a5b5b94f87adb5e287b51eb627
aliMujahid <mhmmdalimujahid@gmail.com>
2019-04-16 17:59:44 +0600

    The same thing but using jinja2

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/<name>')
 def user(name):
     user_agent = request.headers.get('user_agent')
-    return f"<h1> Hello, {name}</h1><p> Your browser is {user_agent}.</p>"
+    return render_template('user.html', name=name, user_agent=user_agent)
 
 @app.route('/')
 def index():
-    return f"<h1>Hello world.</h1>"
+    return render_template('index.html')
 
-    
